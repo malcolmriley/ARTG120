2efb6ad4ac13537c4e4cd07205a0a01cd9338c68
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-05-13 18:47:16 -0700

    Add house to "town" screen

--- 6_Town/JS/Town.js
+++ 6_Town/JS/Town.js
@@ -3,13 +3,19 @@ Town.prototype =
 {
 	preload: function()
 	{
-
+		// TODO: Swap for atlas version.
+		game.load.image("character", "../_Assets/images/character.png");
+		game.load.image("house", "../_Assets/images/house.png");
 	},
 
 	create: function()
 	{
 		game.add.text(0, 0, "Town \n ENTER: MiniGame");
 	  game.stage.backgroundColor = '#ffffff';
+
+		// Create houses
+		group_houses = game.add.group();
+		let house = group_houses.create(0, 0, "house").scale.setTo(0.2, 0.2);
 	},
 
 	update: function()
