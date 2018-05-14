d9f54f42c5802e871f91e672eefcbd5b517c32b4
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-05-13 19:09:03 -0700

    Add houses and character sprites

--- 6_Town/JS/Town.js
+++ 6_Town/JS/Town.js
@@ -10,12 +10,27 @@ Town.prototype =
 
 	create: function()
 	{
-		game.add.text(0, 0, "Town \n ENTER: MiniGame");
-	  game.stage.backgroundColor = '#f0f0f0';
+		game.stage.backgroundColor = '#f0f0f0';
 
-		// Create houses
+		// Create House Grid
 		group_houses = game.add.group();
-		let house = group_houses.create(0, 0, "house").scale.setTo(0.2, 0.2);
+		let startX = 200;
+		let startY = 30;
+		for(let row = 0; row < 3; row += 1) {
+			for (let column = 0; column < 3; column += 1) {
+				var house = group_houses.create(0, 0, "house");
+				house.scale.setTo(0.15, 0.15);
+				house.x = startX + row * (10 + house.width);
+				house.y = startY + column * (10 + house.width);
+			}
+		}
+
+		// Create Player
+		let player = game.add.sprite(0, 0, "character");
+		player.scale.setTo(0.1, 0.1);
+
+		// Create Text Overlay
+		game.add.text(0, 0, "Town \n Click to enter house.");
 	},
 
 	update: function()
