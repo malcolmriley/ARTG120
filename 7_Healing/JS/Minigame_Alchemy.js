cc66333f2228f51d56dc90751beac681a83c091a
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-05-17 17:11:24 -0700

    Add Minigame_Alchemy state to game

--- 7_Healing/JS/Minigame_Alchemy.js
+++ 7_Healing/JS/Minigame_Alchemy.js
@@ -3,12 +3,15 @@ Minigame_Alchemy.prototype =
 {
 	preload: function()
 	{
-
+    // Load Images
+		this.load.path = "../_Assets/images/";
+		this.load.image("backdrop", "old_paper.png");
 	},
 
 	create: function()
 	{
-
+    // Add Backdrop
+    this.game.add.sprite(0, 0, "backdrop");
 	},
 
 	update: function()
