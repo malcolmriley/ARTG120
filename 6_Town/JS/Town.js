cc65c1df69bddf5c4f94f67dddd5e28b6fe9eb15
vanfro <mickeyhinh@gmail.com>
2018-05-24 14:41:00 -0700

    Removed cinematics, started town from scratch

--- 6_Town/JS/Town.js
+++ 6_Town/JS/Town.js
@@ -14,79 +14,60 @@ Town.prototype =
 	create: function()
 	{
 		game.stage.backgroundColor = '#f0f0f0';
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+		game.world.setBounds(0, 0, 1920, 600);
 
 		// Load sounds
 		soundfx_door = game.add.audio("fx_door_creak");
 
-		// Create House Grid
-		group_houses = game.add.group();
-		let startX = 200;
-		let startY = 30;
-		for(let row = 0; row < 3; row += 1) {
-			for (let column = 0; column < 3; column += 1) {
-				let house = group_houses.create(0, 0, "house");
-				scaleDown(house, this); // Set initial sprite scaling
-				house.x = startX + row * (10 + house.width);
-				house.y = startY + column * (10 + house.width);
-				makeButton(house, this, goToInterior, scaleUp, scaleDown);
-			}
+		houses=game.add.group();
+		for(i=0;i<3;i++)
+		{
+			house=houses.create(300*i+400,200,"house");
+			house.scale.set(.25);
+			house.hp=Math.random()*100;
 		}
 
-		// Create Player
-		player = initPlayer();
+		player=game.add.sprite(100,250,"character");
+		player.scale.set(.1);
+		player.anchor.set(.5,0);
+		game.physics.arcade.enable([player,houses]);
 
-		// Create Text Overlay
-		game.add.text(0, 0, "Town \n Click to enter house.");
-
-		// Make cursors for player control
-		cursors = game.input.keyboard.createCursorKeys();
+		game.camera.follow(player);
+		game.camera.deadzone=new Phaser.Rectangle(300,100,200,5);
 	},
 
 	update: function()
 	{
-		// TODO: Better movement system. Right now, left takes precedence over right, and up over down
-		// Also, velocity is preserved in a really dumb way (doesn't reset until ALL keys are released)
-		// For now, the Swensen Bubblegum and Shoestring Method will have to do.
-		let playerSpeed = 150;
-		if (cursors.left.isDown) {
-			player.body.velocity.x = -playerSpeed;
+		if(game.input.keyboard.isDown(Phaser.Keyboard.A))
+		{
+			player.x -= 5;
+			player.scale.set(-.1,.1);
 		}
-		else if (cursors.right.isDown) {
-				player.body.velocity.x = playerSpeed;
+		else if(game.input.keyboard.isDown(Phaser.Keyboard.D))
+		{
+			player.x += 5;
+			player.scale.set(.1);
 		}
-		else if (cursors.up.isDown) {
-			player.body.velocity.y = -playerSpeed;
+		game.physics.arcade.overlap(player,houses,this.checkInput);
+	},
+
+	checkInput: function(player,house)
+	{
+		if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))
+		{
+			console.log(house.hp);
+			goToInterior();
 		}
-		else if (cursors.down.isDown) {
-				player.body.velocity.y = playerSpeed;
-		}
-		else {
-			player.body.velocity.x = 0;
-			player.body.velocity.y = 0;
-		}
+	},
+
+	render: function()
+	{
+		game.debug.body(player);
+		game.debug.physicsGroup(houses);
 	}
-
-	// TODO: Enable travel to individual house
-	// House is a "button" right now for the simple fact of testing the functionality thereof.
-	// More likely, for the final implementation, the player will use some other control for entering an individual house.
 }
 
-function initPlayer() {
-	let instance = game.add.sprite(0, 0, "character");
-  game.physics.arcade.enable(instance);
-	instance.scale.setTo(0.1, 0.1);
-	instance.enableBody = true;
-	instance.body.collideWorldBounds=true;
-	return instance;
-}
-
-function scaleUp(passedSprite, passedReference) {
-	passedSprite.scale.setTo(0.185, 0.185);
-}
-
-function scaleDown(passedSprite, passedReference) {
-	passedSprite.scale.setTo(0.15, 0.15)
-}
 
 function goToInterior() {
 	soundfx_door.play();
