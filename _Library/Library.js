79f27bbe95cf74409d6271a0e4d82de7239ea032
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-02 18:31:08 -0700

    Add RandomizedSound class
    Use it for playing random sound effects!

--- _Library/Library.js
+++ _Library/Library.js
@@ -87,3 +87,54 @@ function storePosition(passedObject) {
 function spritesOverlap(passedFirstSprite, passedSecondSprite) {
     return Phaser.Rectangle.intersects(passedFirstSprite.getBounds(), passedSecondSprite.getBounds());
 }
+
+/**
+ * Special class for randomizing sound playback, for use with sound effects.
+ *
+ * As one might expect, if play() is called, it will select a random sound from its internal array and play it.
+ *
+ * The constructor accepts an arbitray number of arguments:
+ * passedGame - A reference to the game
+ * ... passedSounds - Any number of Strings that you would ordinarily pass to game.add.audio().
+ */
+class RandomizedSound {
+  constructor(passedGame, ... passedSounds) {
+    this.game = passedGame;
+    this.sounds = [];
+    this.playing = 0;
+    for (let iteratedSound of passedSounds) {
+      this.sounds[this.sounds.length] = this.game.add.audio(iteratedSound);
+    }
+  }
+
+  /**
+   * Randomly plays one of the sounds from the internal array.
+   *
+   * Returns the index of that sound as a means of remembering what sound is currently playing.
+   */
+  play() {
+    let index = this.game.rnd.integerInRange(0, this.sounds.length - 1);
+    this.playing = index;
+    this.sounds[index].play();
+    return index;
+  }
+
+  /**
+   * Stops the sound indicated by passedIndex.
+   *
+   * If no argument is supplied, stops the currently-playing sound.
+   */
+  stop(passedIndex) {
+    let index = (passedIndex != undefined) ? passedIndex : this.playing;
+    this.sounds[index].stop();
+  }
+
+  /**
+   * Stops all sounds known to this RandomizedSound.
+   */
+  stopAll() {
+    for (let iteratedSound of this.sounds) {
+      iteratedSound.stop();
+    }
+  }
+}
