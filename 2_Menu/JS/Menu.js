97f0f7408615389f10a01f6fa85bc762d22ac60a
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-06 00:46:29 -0700

    Use relative positioning for elements

--- 2_Menu/JS/Menu.js
+++ 2_Menu/JS/Menu.js
@@ -17,9 +17,18 @@ Menu.prototype =
 	{
 		layer_background = this.game.add.group();
 
+		// Set up temp variables
+		let elementPadding = 50;
+		let center_x = (game.camera.width / 2);
+		let center_y = (game.camera.height / 2);
+
 		// Make title Sprite
-		title = layer_background.create((game.camera.width / 2), (game.camera.height * 0.3), "title");
+		title = layer_background.create(center_x, (game.camera.height * 0.3), "title");
 		centerAnchor(title);
+
+		// Add buttons
+		button_play = createMenuButton(this, center_x, (title.y + elementPadding + (title.height / 2)), layer_background, "text_play", "Town");
+		button_credits = createMenuButton(this, center_x, (button_play.y + elementPadding + button_play.height), layer_background, "text_credits", "Credits");
 
 	  // Add Backdrop
 		createBackdrop(this, "backdrop");
