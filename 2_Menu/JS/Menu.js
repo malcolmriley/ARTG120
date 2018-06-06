3d2a71ee5669afec5bb07a79bdc51ad5a9b7102b
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-05 22:21:28 -0700

    Out with the old
    In with the new

--- 2_Menu/JS/Menu.js
+++ 2_Menu/JS/Menu.js
@@ -8,23 +8,11 @@ Menu.prototype =
 
 	create: function()
 	{
-		game.add.text(0, 0, 'Menu. \n ENTER: Start Game \n C: Credits \n Z: Controls');
-		game.stage.backgroundColor = '#ffffff';
+
 	},
 
 	update: function()
 	{
-		if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER))
-		{
-			game.state.start('Town');
-		}
-		else if(game.input.keyboard.isDown(Phaser.Keyboard.C))
-		{
-			game.state.start('Credits');
-		}
-		else if(game.input.keyboard.isDown(Phaser.Keyboard.Z))
-		{
-			game.state.start('Controls');
-		}
+		
 	}
 }
