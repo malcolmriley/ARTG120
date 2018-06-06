357a232f7b8daf1db892bfce9e3d8c6e03eb23e2
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-06 00:27:55 -0700

    Load assets for menu

--- 2_Menu/JS/Menu.js
+++ 2_Menu/JS/Menu.js
@@ -3,7 +3,12 @@ Menu.prototype =
 {
 	preload: function()
 	{
-
+		this.load.path = "../_Assets/images/";
+		this.load.image("backdrop", "old_paper.png");
+		this.load.image("button", "button.png");
+		this.load.image("title", "title.png");
+		this.load.image("text_credits", "text_credits.png");
+		this.load.image("text_play", "text_play.png");
 	},
 
 	create: function()
