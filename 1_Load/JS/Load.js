52fa8dbfd8f080c96dedf6452770e8c4c9922ec8
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-07 16:03:43 -0700

    Bind loader tasks to more convenient context
    Behold the glory of .bind(). Super gross but super useful.

--- 1_Load/JS/Load.js
+++ 1_Load/JS/Load.js
@@ -11,8 +11,8 @@ Load.prototype =
 
 		// Add tasks to loader
 		let defaultProgress = function(){ return (this.game.load.hasLoaded) ? 1.0 : (this.game.load.progressFloat / 100); };
-		this.loader.addTask(this, loadTextures, "../_Assets/images/", defaultProgress);
-		this.loader.addTask(this, loadAudio, "../_Assets/sounds/", defaultProgress);
+		this.loader.addTask(this.load, loadTextures, "../_Assets/images/", defaultProgress);
+		this.loader.addTask(this.load, loadAudio, "../_Assets/sounds/", defaultProgress);
 
 		// TODO: Add task for loading web fonts?
 
@@ -34,64 +34,64 @@ Load.prototype =
 
 function loadTextures() {
 	// Menu
-	this.load.image("backdrop", "old_paper.png");
-	this.load.image("button", "button.png");
-	this.load.image("title", "title.png");
-	this.load.image("text_credits", "text_credits.png");
-	this.load.image("text_play", "text_play.png");
+	this.image("backdrop", "old_paper.png");
+	this.image("button", "button.png");
+	this.image("title", "title.png");
+	this.image("text_credits", "text_credits.png");
+	this.image("text_play", "text_play.png");
 
 	// Town
-	game.load.image("character", "character.png");
-	game.load.image("house", "house.png");
+	this.image("character", "character.png");
+	this.image("house", "house.png");
 
 	// Alchemy
-	this.load.image("backdrop", "old_paper.png");
-	this.load.image("circle", "circle.png");
-	this.load.image("bowl", "bowl.png");
-	this.load.image("bottle_round", "bottle_round.png");
-	this.load.image("retort", "retort.png");
-	this.load.image("bottle_cork", "cork.png");
-	this.load.image("table", "table.png");
-	this.load.image("shelf", "shelf.png");
-	this.load.image("stand_background", "stand_background.png");
-	this.load.image("stand_foreground", "stand_foreground.png");
-	this.load.image("burner", "burner.png");
-	this.load.image("spill", "spill.png");
-	this.load.spritesheet("liquid_bowl", "liquid_bowl.png", 113, 75);
-	this.load.spritesheet("liquid_bottle",  "liquid_bottle.png", 75, 113);
-	this.load.spritesheet("liquid_retort",  "liquid_retort.png", 150, 75);
+	this.image("backdrop", "old_paper.png");
+	this.image("circle", "circle.png");
+	this.image("bowl", "bowl.png");
+	this.image("bottle_round", "bottle_round.png");
+	this.image("retort", "retort.png");
+	this.image("bottle_cork", "cork.png");
+	this.image("table", "table.png");
+	this.image("shelf", "shelf.png");
+	this.image("stand_background", "stand_background.png");
+	this.image("stand_foreground", "stand_foreground.png");
+	this.image("burner", "burner.png");
+	this.image("spill", "spill.png");
+	this.spritesheet("liquid_bowl", "liquid_bowl.png", 113, 75);
+	this.spritesheet("liquid_bottle",  "liquid_bottle.png", 75, 113);
+	this.spritesheet("liquid_retort",  "liquid_retort.png", 150, 75);
 
 	// Wound
-	this.load.image("bg","old_paper.png");
-	this.load.image("wound0","wound_0.png");
-	this.load.image("wound1","wound_1.png");
-	this.load.image("wound2","wound_2.png");
-	this.load.image("wound3","wound_3.png");
-	this.load.image("bowl","bowl.png");
-	this.load.image("pestle","pestle.png");
-	this.load.image("poultice","resin_huge.png");
+	this.image("bg","old_paper.png");
+	this.image("wound0","wound_0.png");
+	this.image("wound1","wound_1.png");
+	this.image("wound2","wound_2.png");
+	this.image("wound3","wound_3.png");
+	this.image("bowl","bowl.png");
+	this.image("pestle","pestle.png");
+	this.image("poultice","resin_huge.png");
 }
 
 function loadAudio() {
 	// Menu
-	this.load.audio("thump", "thump.wav");
-	this.load.audio("ominous", "ominous.wav");
+	this.audio("thump", "thump.wav");
+	this.audio("ominous", "ominous.wav");
 
 	// Town
-	this.load.audio("fx_door_creak", "door_open.wav");
+	this.audio("fx_door_creak", "door_open.wav");
 
 	// Alchemy
-	this.load.audio("pour", "water_pour.wav");
-	this.load.audio("cork", "cork_out.wav");
-	this.load.audio("clink_0", "bottle_clink_0.wav");
-	this.load.audio("clink_1", "bottle_clink_1.wav");
-	this.load.audio("metal_ping_1", "metal_ping_1.wav");
-	this.load.audio("metal_ping_2", "metal_ping_2.wav");
-	this.load.audio("metal_ping_3", "metal_ping_3.wav");
-	this.load.audio("bottle_break", "bottle_break.wav");
+	this.audio("pour", "water_pour.wav");
+	this.audio("cork", "cork_out.wav");
+	this.audio("clink_0", "bottle_clink_0.wav");
+	this.audio("clink_1", "bottle_clink_1.wav");
+	this.audio("metal_ping_1", "metal_ping_1.wav");
+	this.audio("metal_ping_2", "metal_ping_2.wav");
+	this.audio("metal_ping_3", "metal_ping_3.wav");
+	this.audio("bottle_break", "bottle_break.wav");
 
 	// Wound
-	this.load.audio('background', ['Midnightcem.ogg', "Midnightcem.wav"]);
+	this.audio('background', ['Midnightcem.ogg', "Midnightcem.wav"]);
 }
 
 class LoadHelper {
