aaa3d786888c3ed3a4738718f7f878fb702a6c59
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-06 21:31:06 -0700

    Add boot state
    - It loads the things needed for the load state.

---
+++ 1_Load/JS/Boot.js
@@ -0,0 +1,25 @@
+var Boot = function(game) {};
+Boot.prototype =
+{
+	preload: function()
+	{
+			// Load stuff needed for load screen
+			this.load.path = "../_Assets/images/";
+      this.game.load.image("progressbar", "progressbar.png");
+      this.game.load.image("progressbar_background.png");
+
+      // Reset load path to prevent breaking states that don't set it
+      // TODO: Remove when these issues are removed from the other states
+      this.load.path = "";
+	},
+
+	create: function()
+	{
+    game.state.start("Load");
+	},
+
+	update: function()
+	{
+
+	}
+}
