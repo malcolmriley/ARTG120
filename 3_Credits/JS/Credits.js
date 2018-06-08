b5bc50632a66eab66516550c4a47992b6c9e3d8f
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-07 20:55:40 -0700

    Create CreditsObject class

--- 3_Credits/JS/Credits.js
+++ 3_Credits/JS/Credits.js
@@ -15,6 +15,11 @@ Credits.prototype =
 		// Add text layer
 		this.layer_text = this.game.add.group();
 
+		let center_x = (game.camera.width / 2);
+		let center_y = (game.camera.height / 2);
+
+		new CreditsObject(this.game, center_x, center_y, "TEST", "Did A Thing:\nAlsoDid:", "Firstname Lastname III\nArble Garble")
+
 		// Add backdrop
 		createBackdrop(this, "backdrop");
 	},
@@ -22,5 +27,55 @@ Credits.prototype =
 	update: function()
 	{
 
+	}
+}
+
+class CreditsObject {
+	constructor(passedGameReference, passedXPosition, passedYPosition, passedHeader, passedLeftColumn, passedRightColumn) {
+		let headerStyle = {
+			font : "bold", // TODO
+			align : "center",
+			fontSize: 32,
+		};
+		let normalStyle = {
+			font : "normal", // TODO
+			align : "center",
+			fontSize: 26,
+		};
+		let makeIfPresent = function(passedText, passedStyle) {
+			if (passedText) {
+				let instance = passedGameReference.add.text(passedXPosition, passedYPosition, passedText, passedStyle);
+				centerAnchor(instance);
+				return instance;
+			}
+			return null;
+		}
+		this.header = makeIfPresent(passedHeader, headerStyle);
+		this.column_left = makeIfPresent(passedLeftColumn, normalStyle);
+		this.column_right = makeIfPresent(passedRightColumn, normalStyle);
+
+		// Position all elements
+		this.x = passedXPosition;
+		this.y = passedYPosition;
+	}
+
+	set alpha(passedValue) {
+		if (this.header) { this.header.alpha = passedValue };
+		if (this.column_left) { this.column_left.alpha = passedValue };
+		if (this.column_right) { this.column_right.alpha = passedValue };
+	}
+
+	set x(passedValue) {
+		if (this.header) { this.header.x = passedValue };
+		let offset = 0;
+		if (this.column_left && this.column_right) { offset = 150 };
+		if (this.column_left) { this.column_left.x = passedValue - (offset) };
+		if (this.column_right) { this.column_right.x = passedValue + (offset) };
+	}
+
+	set y(passedValue) {
+		if (this.header) { this.header.y = passedValue - (this.header.height / 2) };
+		if (this.column_left) { this.column_left.y = passedValue + (this.column_left.height / 2) };
+		if (this.column_right) { this.column_right.y = passedValue + (this.column_right.height / 2) };
 	}
 }
