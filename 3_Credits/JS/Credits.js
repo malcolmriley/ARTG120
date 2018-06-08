86e3138b31613c5fb3578630e63c81566a5e1021
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-07 20:02:38 -0700

    Add text layer and backdrop

--- 3_Credits/JS/Credits.js
+++ 3_Credits/JS/Credits.js
@@ -11,6 +11,12 @@ Credits.prototype =
 		// Add escape functionality
 		this.key_escape = this.game.input.keyboard.addKey(Phaser.Keyboard.ESC);
 		this.key_escape.onDown.add(function(){ game.state.start("Menu"); });
+
+		// Add text layer
+		this.layer_text = this.game.add.group();
+
+		// Add backdrop
+		createBackdrop(this, "backdrop");
 	},
 
 	update: function()
