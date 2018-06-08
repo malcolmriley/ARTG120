be8da9cc43f78bbf70d346a211cdb0384f5662c7
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-07 17:59:14 -0700

    Add visuals to gameover screen

--- 8_GameOver/JS/GameOver.js
+++ 8_GameOver/JS/GameOver.js
@@ -8,7 +8,20 @@ GameOver.prototype =
 
 	create: function()
 	{
-		
+		// Add UI Layer
+		layer_ui = this.game.add.group();
+
+		// Add screen elements
+		let elementPadding = 50;
+		let center_x = (game.camera.width / 2);
+		let center_y = (game.camera.height / 2);
+		this.grave = layer_ui.create(center_x, center_y, "grave");
+		centerAnchor(this.grave);
+
+		this.button_quit = createMenuButton(this, center_x, (this.grave.y + elementPadding + (this.grave.height / 2)), layer_ui, "text_gameover", "Menu");
+
+		// Add backdrop
+		createBackdrop(this, "backdrop");
 	},
 
 	update: function()
