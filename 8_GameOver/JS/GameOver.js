730d00f1360db749c9595c23e0c53db59f346946
Malcolm Riley <malcolmriley@users.noreply.github.com>
2018-06-07 17:32:04 -0700

    Out with the old...

--- 8_GameOver/JS/GameOver.js
+++ 8_GameOver/JS/GameOver.js
@@ -8,21 +8,11 @@ GameOver.prototype =
 
 	create: function()
 	{
-		game.add.text(0, 0, "GameOver \n ENTER: Menu \n R: Town (Restart)");
-		game.stage.backgroundColor = '#ffffff';
+		
 	},
 
 	update: function()
 	{
-		if(game.input.keyboard.isDown(Phaser.Keyboard.ENTER))
-		{
-			diff=0;
-			game.state.start('Menu');
-		}
-		else if(game.input.keyboard.isDown(Phaser.Keyboard.R))
-		{
-			diff=0;
-			game.state.start('Town');
-		}
+
 	}
 }
