a5e8e103009e4595686a6b113cc6b0aea54e2c01
Jesus Hernandez <30272595+JjayaitchH@users.noreply.github.com>
2018-06-07 23:28:01 -0700

    Added comments for readability

--- 6_Town/JS/Town.js
+++ 6_Town/JS/Town.js
@@ -22,8 +22,9 @@ Town.prototype =
 
 	create: function()
 	{
-
+		//as always, phyics
 		game.physics.startSystem(Phaser.Physics.ARCADE);
+		//temp background
 		game.stage.backgroundColor = '#f0f0f0';
 
 		// Load sounds
@@ -31,18 +32,19 @@ Town.prototype =
 
 		// Create Player
 		this.player = initPlayer();
-		//scaleDown(this.player);
 
+		/*grab the width and height of the house image to
+		use for positioning houses in a grid, round numbers*/
 		var width = game.cache.getImage("house").width;
 		width = Math.ceil(width * .10);
 		var height = game.cache.getImage("house").height;
 		height = Math.ceil(height * .10);
 
-		// Create House Grid
-		//this.group_houses = game.add.group();
+		//House group
 		this.group_houses = game.add.group();
 
-		//makeHouse(group_houses);
+		/*This right here reads in the array and creates an evenly 
+		spaced grid for houses*/
 		for(var i = 0; i < town.length; i++)
 		{
 			var row = town[i];
@@ -55,7 +57,9 @@ Town.prototype =
 			}
 		}
 
+		//send the houses to back b/c they appear above player
 		game.world.sendToBack(this.group_houses);
+		//give the houses group physics
 		game.physics.arcade.enable(this.group_houses);
 		// Create Text Overlay
 		game.add.text(0, 0, "Town \n Click to enter house.");
@@ -63,9 +67,7 @@ Town.prototype =
 
 	update: function()
 	{
-		// TODO: Better movement system. Right now, left takes precedence over right, and up over down
-		// Also, velocity is preserved in a really dumb way (doesn't reset until ALL keys are released)
-		// For now, the Swensen Bubblegum and Shoestring Method will have to do.
+		//player movement
 		let playerSpeed = 150;
 		if(game.input.keyboard.isDown(Phaser.Keyboard.A))
 		{
@@ -93,6 +95,7 @@ Town.prototype =
 	// More likely, for the final implementation, the player will use some other control for entering an individual house.
 }
 
+//boilerplate for initializing a player
 function initPlayer() {
 	let instance = game.add.sprite(0, 0, "character");
   	game.physics.arcade.enable(instance);
@@ -102,6 +105,7 @@ function initPlayer() {
 	return instance;
 }
 
+//this creates a house by calling the House prefab and adding it to house group
 function makeHouse(group, height, width, i, j, player)
 {
 	let house = new House(game, "house", player, 100, height, width, i, j);
@@ -109,20 +113,33 @@ function makeHouse(group, height, width, i, j, player)
 	group.add(house);
 }
 
+//a function to scale up a sprite
 function scaleUp(passedSprite, passedReference) {
 	passedSprite.scale.setTo(0.185, 0.185);
 }
 
+//a function to scale down a sprite
 function scaleDown(passedSprite) {
 	passedSprite.scale.setTo(0.10, 0.10)
 }
 
+/* NOTE TO SELF: Make the scale functions more universal by allowing them to take in
+ant sclae factor. But also this can easily be done in one like, I don't know why we need these functions
+but I'm using them anyways so I guess I'll keep them for now
+*/
+
+/**This is supposed to be the function for entering a house
+ * but I don't think there is much of a use for it because this is now being handeled in the 
+ * House prefab update (but it can also be handeled in the main update; I guess whereever it 
+ * will be more convenient in the end)
+ */
 function goToInterior() {
 	soundfx_door.play();
 	// TODO: Transition to other minigames as well
 	game.state.start("Minigame_Wound");
 }
 
+//Ahhhh yes the house prefab. No prefab folders because this is actually the only one so I just left it in here
 function House(game, key, player, health, height, width, i, j)
 {
 	Phaser.Sprite.call(this, game, j * width, i * height, key);
@@ -133,6 +150,10 @@ function House(game, key, player, health, height, width, i, j)
 	this.body.collideWorldBounds = true;
 	this.enableBody = true;
 
+	/**The reason player is being passed is because there are some things I want 
+	 * each house itself to individually do when they interact with a player that might cause 
+	 * some error if done using the entire house group
+	 */
 	this.player = player;
 
 
@@ -146,6 +167,13 @@ House.prototype.constructor = House;
 
 House.prototype.update = function()
 {
+	/**Check to see if there is overlap between the player and this house
+	 * and if there is then scale up by .2 to let the player know they can
+	 * do something to this house i.e. enter it
+	 * 
+	 * Once they enter they then get sent to one of two minigames. When the player
+	 * walks away from the house it scales back down to normal
+	 */
 	if(game.physics.arcade.overlap(this, this.player))
 	{
 			this.scale.setTo(.2);
