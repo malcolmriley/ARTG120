b52cd26bce21bb5cf5500cf78f3dd66879bbf18e
vanfro <mickeyhinh@gmail.com>
2018-06-10 23:30:11 -0700

    Stuff overwritten :(, player loc not saved

--- 6_Town/JS/Town.js
+++ 6_Town/JS/Town.js
@@ -1,15 +1,6 @@
-var town = [
-	[0, 0, 0, 0, 0, 0, 0],
-	[0, 1, 2, 1, 2, 1, 0],
-	[0, 2, 2, 2, 2, 2, 0],
-	[0, 1, 2, 1, 2, 1, 0],
-	[0, 2, 2, 2, 2, 2, 0],
-	[0, 1, 2, 1, 2, 1, 0],
-	[0, 0, 0, 0, 0, 0, 0]
-];
+health=[[100,100,100],[100,100,100],[100,100,100]];
+dead=0;
 
-var lostHouses = 0;
-var listMinigame = false;
 var Town = function(game) {};
 Town.prototype =
 {
@@ -20,47 +11,37 @@ Town.prototype =
 
 	create: function()
 	{
-		//as always, phyics
+		// initializes physics and world
 		game.physics.startSystem(Phaser.Physics.ARCADE);
-		//temp background
-		game.stage.backgroundColor = '#f0f0f0';
 
 		// Load sounds
 		soundfx_door = game.add.audio("fx_door_creak");
 
-		// Create Player
-		player = initPlayer();
+		//Houses group
+		houses=game.add.group();
 
-		/*grab the width and height of the house image to
-		use for positioning houses in a grid, round numbers*/
-		var width = game.cache.getImage("house").width;
-		width = Math.ceil(width * .5);
-		var height = game.cache.getImage("house").height;
-		height = Math.ceil(height * .3);
-
-		//House group
-		group_houses = game.add.group();
-
-		/*This right here reads in the array and creates an evenly
-		spaced grid for houses*/
-		for(var i = 0; i < town.length; i++)
+		for(i=0;i<health.length;i++)
 		{
-			var row = town[i];
-			for(var j = 0; j < row.length; j++)
+			for(j=0;j<health[i].length;j++)
 			{
-				if(town[i][j] == 1)
-				{
-					makeHouse(group_houses, height, width, i, j, player);
-				}
+				house=houses.create(250*i+150,200*j+40,"house");
+				house.scale.set(.5);
+				house.row=i;
+				house.col=j;
+				house.hp=game.add.text(250*i+150,200*j+20,'health: '+health[i][j]);
 			}
 		}
 
-		//send the houses to back b/c they appear above player
-		game.world.sendToBack(group_houses);
-		//give the houses group physics
-		game.physics.arcade.enable(group_houses);
-		// Create Text Overlay
-		//game.add.text(0, 0, "Town \n Click to enter house.");
+		// Create Player
+		player=initPlayer();
+
+		// enables physics bodies for all sprites
+		game.physics.arcade.enable([player,houses]);
+
+		// adds text to display deaths
+		toll=game.add.text(10,10,"Deaths: "+dead);
+
+		createBackdrop(this, "backdrop");
 	},
 
 	update: function()
@@ -68,44 +49,84 @@ Town.prototype =
 		//player movement
 		if(game.input.keyboard.isDown(Phaser.Keyboard.A))
 		{
-			player.x -= 5;
+			player.x-=5;
+			player.scale.set(-1,1);
 		}
 		else if(game.input.keyboard.isDown(Phaser.Keyboard.D))
 		{
-			player.x += 5;
+			player.x+=5;
+			player.scale.set(1);
 		}
 		if(game.input.keyboard.isDown(Phaser.Keyboard.W))
 		{
-			player.y -= 5;
+			player.y-=5;
 		}
 		else if(game.input.keyboard.isDown(Phaser.Keyboard.S))
 		{
-			player.y += 5;
+			player.y+=5;
 		}
-		else {
-			player.body.velocity.x = 0;
-			player.body.velocity.y = 0;
+
+		// checks if player inputs button when on house
+		game.physics.arcade.overlap(player,houses,this.checkInput);
+		// checks each house sprite and updates their properties
+		houses.forEach(this.updateHouse,this);
+
+		// updates death toll text
+		toll.text="Deaths: "+dead;
+
+		if(Math.random()<.2)
+		{
+			health[Math.floor(Math.random()*3)][Math.floor(Math.random()*3)]-=1;
 		}
-	}
+
+		// ends the game when enough has died
+		if(dead==3)
+		{
+			game.state.start('GameOver');
+		}
+
+	},
+
+	checkInput: function(player,house)
+	{
+		// checks if player is interacting with houses
+		if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))
+		{
+			// simple function to play sound and begin minigames
+			goToInterior(house.row);
+		}
+	},
+
+	updateHouse: function(house)
+	{
+		// if house is dead
+		if(health[house.row][house.col]<1)
+		{
+			// disable interaction and remove text
+			house.body.enable=false;
+			house.hp.text='';
+			// increases death count when health hits 0
+			if(health[house.row][house.col]==0)
+			{
+				dead++;
+				// sets health to negative so dead doesn't increase for multiple ticks
+				health[house.row][house.col]=-1;	
+			}
+		}
+		else
+		{
+			// otherwise, update health text
+			house.hp.text='health: '+health[house.row][house.col];
+		}
+},
 //,render:function(){game.debug.body(this.player);game.debug.physicsGroup(group_houses);}
 }
 
 //boilerplate for initializing a player
 function initPlayer() {
-	let instance = game.add.sprite(0, 0, "character");
-  	game.physics.arcade.enable(instance);
-	instance.scale.setTo(1);
-	instance.body.enable = true;
-	instance.body.collideWorldBounds=true;
+	instance=game.add.sprite(50,250,"character");
+	instance.anchor.set(.5,0);
 	return instance;
-}
-
-//this creates a house by calling the House prefab and adding it to house group
-function makeHouse(group, height, width, i, j, player)
-{
-	let house = new House(game, "house", player, height, width, i, j);
-	game.add.existing(house);
-	group.add(house);
 }
 
 //a function to scale up a sprite
@@ -118,86 +139,9 @@ function scaleDown(passedSprite) {
 	passedSprite.scale.setTo(.4);
 }
 
-//Ahhhh yes the house prefab. No prefab folders because this is actually the only one so I just left it in here
-function House(game, key, player, height, width, i, j)
-{
-	Phaser.Sprite.call(this, game, j * width, i * height, key);
-
-	this.anchor.set(0.5);
-
-	game.physics.arcade.enable(this, this.player);
-	//this.body.collideWorldBounds = true;
-	this.body.enable = true;
-	//this.body.immovable = true;
-
-	this.maxHealth = 100;
-	this.health = game.rnd.integerInRange(50, 70);
-	this.takeDamage = game.rnd.integerInRange(2, 5);
-	this.alive = true;
-
-	this.player = player;
-	this.enter = false;
-
-	//death clock, every 3 seconds damage function is called
-	this.timer = game.time.create(false);
-	this.timer.loop(3000, damage, this);
-	this.timer.start();
-
-	scaleDown(this);
-
-	this.hp = game.add.text(this.x, (this.y - (this.width / 2) - 20),'Health: ' + this.health, {font: "20px"});
-}
+//this.hp = game.add.text(this.x, (this.y - (this.width / 2) - 20),'Health: ' + this.health, {font: "20px"})
 
 function goToInterior(something) {
 	soundfx_door.play();
-	// TODO: Transition to other minigames as well
-	group_houses.visible=false;
-	group_houses.forEach(function(house){house.hp.visible=false;},this);
-	player.visible=false;
-	game.state.start(choose("Minigame_Alchemy", "Minigame_Wound"),false,false,something);
-}
-
-House.prototype = Object.create(Phaser.Sprite.prototype);
-House.prototype.constructor = House;
-
-House.prototype.update = function()
-{
-	//updates text
-	this.hp.setText('Health: ' + this.health, {font: "20px"});
-
-	//check for overlap and if house is alive scale up for funsies, and enter house to play minigame
-	if(game.physics.arcade.overlap(this, this.player) && this.alive == true)
-	{
-			this.scale.setTo(.6);
-			if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))
-			{
-				this.enter = true;
-        goToInterior(group_houses);
-			}
-
-	}
-	//scale down when no overlap
-	else
-	{
-		scaleDown(this);
-	}
-
-	//if house was entered and is no longer alive disable the physics body
-	if(this.enter == true && this.alive == false)
-	{
-		this.body.enable = false;
-		lostHouses++;
-	}
-}
-
-//gradual damage to house, once it dies kill its timer
-var damage = function()
-{
-	this.health -= this.takeDamage;
-	if(this.health <= 0)
-	{
-		this.alive = false;
-		this.timer.destroy();
-		this.health = 0;
-	}
+	game.state.start(choose("Minigame_Alchemy", "Minigame_Wound"),true,false);
 }
